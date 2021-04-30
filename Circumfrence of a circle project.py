93305715611f3d48425b24f6c21450133e3701d0
NotFineO-Saur <82458996+NotFineO-Saur@users.noreply.github.com>
2021-04-30 14:58:40 -0600

    text document is code backed up as text
    i mean title doesn't lie

---
+++ Circumfrence of a circle project.py
@@ -0,0 +1,17 @@
+#Ask user for thier radius (in float)
+#tell them thier radius (use as a yes no question later down the line)
+#Caculate circumfrence (via the equation 2(pi)r)
+#tell the user the result
+#Try to lose my C++ Superiority Complex lol
+def circlemoment():
+    Input_a = float(input("What is the radius of the circle?: "))
+    print("Radius:", Input_a)
+    Pi_stuff = Input_a * 3.14
+    print("The Circumfrence of your circle is:", 2 * Pi_stuff)
+
+while True:
+    circlemoment()
+#-Hopefully will loop its self when I figure out how it loops in a future update-
+#-Check Github for updates!-
+#Nevermind, got it to loop
+
